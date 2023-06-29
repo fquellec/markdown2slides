8412162b5f3aa78e9624478a020e2bd0d09c5ec3
fquellec <francois.quellec@gmail.com>
2023-06-29 23:02:02 +0200

    update base path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/markdown2slides/",
   plugins: [
     vue({ 
       template: { transformAssetUrls }
